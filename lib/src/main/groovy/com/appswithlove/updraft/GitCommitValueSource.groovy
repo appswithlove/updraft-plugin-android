17a4321229f59972e55d8655ab9ee4b3f7e90ff7
Vladislav Frolov <f.vladislav.k@gmail.com>
2025-07-18 11:50:12 +0200

    Add Gradle Configuration Cache support

---
+++ lib/src/main/groovy/com/appswithlove/updraft/GitCommitValueSource.groovy
@@ -0,0 +1,19 @@
+package com.appswithlove.updraft
+
+import org.gradle.api.provider.ValueSource
+import org.gradle.api.provider.ValueSourceParameters
+
+abstract class GitCommitValueSource implements ValueSource<String, ValueSourceParameters.None> {
+    String obtain() {
+        def error = null
+        def bashUrl = "git rev-parse HEAD"
+        def command = bashUrl.execute()
+        def outputUrlStream = new StringBuffer()
+        command.waitForProcessOutput(outputUrlStream, error)
+        if (error == null && outputUrlStream.size() > 0) {
+            return outputUrlStream.toString()
+        } else {
+            return ""
+        }
+    }
+}
---
+++ lib/src/main/groovy/com/appswithlove/updraft/GitUrlValueSource.groovy
@@ -0,0 +1,19 @@
+package com.appswithlove.updraft
+
+import org.gradle.api.provider.ValueSource
+import org.gradle.api.provider.ValueSourceParameters
+
+abstract class GitUrlValueSource implements ValueSource<String, ValueSourceParameters.None> {
+    String obtain() {
+        def error = null
+        def bashUrl = "git config --get remote.origin.url"
+        def command = bashUrl.execute()
+        def outputUrlStream = new StringBuffer()
+        command.waitForProcessOutput(outputUrlStream, error)
+        if (error == null && outputUrlStream.size() > 0) {
+            return outputUrlStream.toString()
+        } else {
+            return ""
+        }
+    }
+}
