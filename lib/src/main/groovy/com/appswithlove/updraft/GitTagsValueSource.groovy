4474ba439cbffbfb16fc9ec0d44afedb36151f16
Vladislav Frolov <f.vladislav.k@gmail.com>
2025-07-23 11:33:09 +0200

    fix: exception thrown when git fails to execute a command

--- lib/src/main/groovy/com/appswithlove/updraft/GitTagsValueSource.groovy
+++ lib/src/main/groovy/com/appswithlove/updraft/GitTagsValueSource.groovy
@@ -5,14 +5,18 @@ import org.gradle.api.provider.ValueSourceParameters
 
 abstract class GitTagsValueSource implements ValueSource<String, ValueSourceParameters.None> {
     String obtain() {
-        def error = null
-        def bashUrl = "git describe --tags"
-        def command = bashUrl.execute()
-        def outputUrlStream = new StringBuffer()
-        command.waitForProcessOutput(outputUrlStream, error)
-        if (error == null && outputUrlStream.size() > 0) {
-            return outputUrlStream.toString()
-        } else {
+        try {
+            def error = null
+            def bashUrl = "git describe --tags"
+            def command = bashUrl.execute()
+            def outputUrlStream = new StringBuffer()
+            command.waitForProcessOutput(outputUrlStream, error)
+            if (error == null && outputUrlStream.size() > 0) {
+                return outputUrlStream.toString()
+            } else {
+                return ""
+            }
+        } catch (Exception ignored) {
             return ""
         }
     }
