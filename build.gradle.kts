d088eaff0e72136789d64d8dea54818eb8c9ea89
Vladislav Frolov <f.vladislav.k@gmail.com>
2026-01-20 13:47:05 +0100

    fix sample app's dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,10 +11,11 @@ buildscript {
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.updraft) apply false
     alias(libs.plugins.maven.publish) apply false
     alias(libs.plugins.gradle.publish) apply false
     alias(libs.plugins.coveralls)
     alias(libs.plugins.kotlin.jvm) apply false
     id("signing")
+    // Define Updraft plugin
+    // alias(libs.plugins.updraft) apply false
 }
